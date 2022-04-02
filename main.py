f829cf89852ac832c1e275a18c564fbb72ef7961
Juan Ignacio Londra <46230600+jualon92@users.noreply.github.com>
2022-04-02 11:19:04 +0000

    logica a la hora de mostrar numeros muy grandes o chicos

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ val = True
 presentes = []
 usuarios_ignorar = []
 cMensajes = 0
+
 bot = commands.Bot(command_prefix='!')
 
 
@@ -53,6 +54,7 @@ async def tirar_dado(ctx, *argv):
 
 @bot.command(name="plex", help="precio de plex en Eve Online desde api ")
 async def obtener_plex_precio(ctx):
+    data_formato = "percentile"
     def procesar_plex(numero): # es convencion que el precio sea 500 unidades de plex, en 1B. ej:1.3B
       return (numero * 500) /  1000000000  
     # obtengo objeto Response
@@ -62,19 +64,22 @@ async def obtener_plex_precio(ctx):
     lista_sell = data[0].get("sell")
     
   #convencion es hablar de  1.3Billones
-    precio_compra_avg = procesar_plex(lista_buy.get("wavg"))   
-    precio_venta_avg = procesar_plex(lista_sell.get("wavg"))
+    precio_compra_avg = procesar_plex(lista_buy.get(data_formato))   
+    precio_venta_avg = procesar_plex(lista_sell.get(data_formato))
   
   #string, html
-    mensaje_header = "Jita (4:4) -  x 500u Promedio Semanal"
+    mensaje_header = "Jita (4:4) -  x 500u "
     mensaje_plex_compra = f"\nPlex para la Compra: {precio_compra_avg:.2f} B"
     mensaje_plex_venta = f"\nPlex para la Venta:    {precio_venta_avg:.2f} B"
     await ctx.send(mensaje_header + mensaje_plex_venta + mensaje_plex_compra)
 
 
-
+ 
 @bot.command(name="precio", help="precio de item en Eve Online desde api ")
 async def obtener_item_precio(ctx, *args): 
+    data_formato = "fivePercent" # 95th.  podria ser avg, o wavg 
+     
+  
     nombre_item = " ".join(args) 
     async def obtener_id():
     # obtengo id del item a buscar en api de id
@@ -85,33 +90,36 @@ async def obtener_item_precio(ctx, *args):
     data_item = await obtener_id() 
     id_item = data_item.get("typeID")
     nombre_obtenido = data_item.get("typeName")
-  
+    
     #obtengo info del item
     data_mercado = await get_data(f"https://api.evemarketer.com/ec/marketstat/json?typeid={id_item}&usesystem=30000142")
         
-    precio_compra_avg = int(data_mercado[0].get("buy").get("avg"))
-    precio_venta_avg = int(data_mercado[0].get("sell").get("avg"))
+    precio_compra_data = float(data_mercado[0].get("buy").get(data_formato))
+    precio_venta_data = float(data_mercado[0].get("sell").get(data_formato))
 
-
-  
-    #logica, estaria mejor en import
+    
+     # estaria mejor en import,  utilizar oop,  no me gusta maraña de if
     if (nombre_obtenido == "bad item"):  #si no existe aisar al usuario que escribio mal
       await ctx.send(f"item con nombre {nombre_item} no encontrado :(  ")
+       
+    elif precio_venta_data > 0 : #si es encontrado, devolver valor de compra y venta en market hub
+      if precio_venta_data >= 1000000: #si es muy grande, setear numero menos digitos para mejor display 
+        precio_venta_data = str(round(precio_venta_data / 1000000)) + " Millones"
+        precio_compra_data = str(round(precio_compra_data / 1000000)) + " Millones"
+      await ctx.send(f"Precio de {nombre_obtenido}\nPrecio venta: {precio_venta_data} \nPrecio compra: {precio_compra_data}")
+     
       
-    elif precio_venta_avg > 0 : #si es encontrado, devolver valor de compra y venta
-       await ctx.send(f"Precio promedio x unidad de {nombre_obtenido}\nPrecio venta: {precio_venta_avg}  \nPrecio compra: { precio_compra_avg}")
-      
-    else: #si existe pero api devuelve valor 0, mejor buscar en contratos p2p
+    else: #si valor de venta devuelve 0 => se trata de un item exotico, agrandar busqueda
       async with aiohttp.ClientSession() as session:   
-        async with session.get(f"https://api.contractsappraisal.com/v1/prices/{id_item}?include_private=false&bpc=false&security=lowsec") as response: 
-          if (response.status != 200):
-             await ctx.send("item no encontrado")
-          else:
-            dato_contrato = await response.json()  #custom
-            precio_venta_contrato = dato_contrato.get("median")
-            await ctx.send(f"Consultando contratos precio promedio de {nombre_item}\nPrecio venta: {int(precio_venta_contrato) / 1000000000 } B")
+        async with session.get(f"https://api.evemarketer.com/ec/marketstat/json?typeid={id_item}") as response: 
+          dato_all = await response.json()  #custom
+          precio_venta_todas = reduce_cifras(dato_all[0].get("sell").get(data_formato))
+          precio_compra_todas = reduce_cifras(dato_all[0].get("buy").get(data_formato))
+          await ctx.send(f"Precio todas las regiones de {nombre_item}\nPrecio venta: {precio_venta_todas} B \n Precio Compra {precio_compra_todas} B")
     
 
+def reduce_cifras(palabra): #  cuando se trata de billones, se utiliza 1.3b 
+  return round(int(palabra) / 1000000000,2)
 
 def separar_partes(item, n_partes):
     item_str = str(item)
